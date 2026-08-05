5d965c6b895844a2ebcea5f6fd5b5a36e7944bf1
Ausb-Wood <brandon.wood@damago.eu>
2026-08-05 11:44:02 +0200

    Remove Kubernetes provider and training namespace
    Removed Kubernetes provider configuration and training namespace resource.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,14 +2,3 @@ provider "kubernetes" {
   config_path    = pathexpand("~/.kube/config")
   config_context = "minikube"
 }
-
-resource "kubernetes_namespace_v1" "training" {
-  metadata {
-    name = var.namespace
-    labels = {
-      environment = "training"
-      managed-by  = "opentofu"
-      application = var.application_name
-    }
-  }
-}
